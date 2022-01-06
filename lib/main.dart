7decf022f5f27b21a01deee43e75f52940940413
maeva <maeganelys@gmail.com>
2022-01-06 11:49:15 +0400

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red[50],
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children:[
+            CircleAvatar(radius: 100,
+              backgroundImage: AssetImage("images/yae.jpg"),
+            ),
+                Text ("Mae Yae",
+                    style:
+                    TextStyle(
+                        fontFamily: "Pacifico",
+                        fontSize:  20,
+                      color: Colors.purple,
+                    ),
+                ),
+                Text ("Test de police",
+                  style:
+                  TextStyle(
+                    fontFamily: "SourceSansPro",
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5,
+                    color: Colors.teal.shade100
+                  ),
+                )
+              ],
+            ),
+          ),
+        ) ,
+      ),
+    );
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
